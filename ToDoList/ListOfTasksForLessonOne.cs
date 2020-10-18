01964597aba3eaa98c903ea5da6ae57cd3dcfa44
Marek <vat77@tlen.pl>
2020-10-18 10:56:32 +0200

    change text of tasks list

--- ToDoList/ListOfTasksForLessonOne.cs
+++ ToDoList/ListOfTasksForLessonOne.cs
@@ -15,10 +15,10 @@ namespace First.ToDoList
         public void PrintLessonOneTasks()
         {
             List<string> lessonOneTasks = new List<string>();
-            lessonOneTasks.Add("Wypisz w konsoli swoje imię i nazwisko, wykorzystując w tym celu wszystkie znane Ci metody wyświetlania komunikatów.");
-            lessonOneTasks.Add("Zad. 2. Utwórz dwie zmienne liczbowe i przypisz im wartości. Wyświetl w konsoli wyniki dodawania, odejmowania, mnożenia oraz dzielenia. Niech druga liczba nie będzie zerem. Użyj typu danych double. ");
-            lessonOneTasks.Add("Utwórz dwie zmienne liczbowe i przypisz im wartości podane poprzez konsolę. Wyświetl w konsoli wyniki dodawania, odejmowania, mnożenia oraz dzielenia. Niech druga liczba nie będzie zerem. Użyj typu danych double.");
-            lessonOneTasks.Add("Utwórz dwie zmienne liczbowe typu double. Dla każdego z działań (dodawanie, odejmowanie, mnożenie i dzielenie) przypisz z konsoli nowy zestaw liczb oraz wypisz ich wyniki w konsoli w formie pełnych działań arytmetycznych($”{ }”).");
+            lessonOneTasks.Add("1. Wypisz w konsoli swoje imię i nazwisko, wykorzystując w tym celu wszystkie znane Ci metody wyświetlania komunikatów.");
+            lessonOneTasks.Add("2. Utwórz dwie zmienne liczbowe i przypisz im wartości. Wyświetl w konsoli wyniki dodawania, odejmowania, mnożenia oraz dzielenia. Niech druga liczba nie będzie zerem. Użyj typu danych double. ");
+            lessonOneTasks.Add("3. Utwórz dwie zmienne liczbowe i przypisz im wartości podane poprzez konsolę. Wyświetl w konsoli wyniki dodawania, odejmowania, mnożenia oraz dzielenia. Niech druga liczba nie będzie zerem. Użyj typu danych double.");
+            lessonOneTasks.Add("4. Utwórz dwie zmienne liczbowe typu double. Dla każdego z działań (dodawanie, odejmowanie, mnożenie i dzielenie) przypisz z konsoli nowy zestaw liczb oraz wypisz ich wyniki w konsoli w formie pełnych działań arytmetycznych($”{ }”).");
 
             Console.WriteLine();
             foreach(string lesson in lessonOneTasks)
